01fae15015274bd3135aed9583ab47e70c3bfda2
santipu <santipujolsau@gmail.com>
2022-11-10 18:24:01 +0100

    intial commit

---
+++ hardhat.config.js
@@ -0,0 +1,79 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+require("@openzeppelin/hardhat-upgrades")
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const REPORT_GAS = process.env.REPORT_GAS || false
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.4.24" }],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: "goerli",
+                chainId: 5,
+                urls: {
+                    apiURL: "https://api-goerli.etherscan.io/api",
+                    browserURL: "https://goerli.etherscan.io",
+                },
+            },
+        ],
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["Box"],
+    },
+    mocha: {
+        timeout: 300000, // 300 secs max
+    },
+}
